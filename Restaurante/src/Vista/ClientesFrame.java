1b115c35aa975987afe6aade13fe6bc7fe97e2bb
Jesus-Iniesta <iniestavalverdejesus4@gmail.com>
2024-11-29 15:27:48 -0600

    Cambios en conexion y clienteFrame

--- Restaurante/src/Vista/ClientesFrame.java
+++ Restaurante/src/Vista/ClientesFrame.java
@@ -37,8 +37,14 @@ public class ClientesFrame extends javax.swing.JFrame {
         TxtApellidoMaterno = new javax.swing.JTextField();
         txtApellidoPaterno = new javax.swing.JTextField();
         TxtTelefonoCli = new javax.swing.JTextField();
-        TxtDireccionCli = new javax.swing.JTextField();
+        txtPaisCLT = new javax.swing.JTextField();
         TxtCorreoCli = new javax.swing.JTextField();
+        LbDireccionCli1 = new javax.swing.JLabel();
+        TxtCalleCli1 = new javax.swing.JTextField();
+        LbDireccionCli2 = new javax.swing.JLabel();
+        txtColoniaCLT1 = new javax.swing.JTextField();
+        LbDireccionCli3 = new javax.swing.JLabel();
+        txtCPCLT = new javax.swing.JTextField();
         jPanel2 = new javax.swing.JPanel();
         BtnGuardarCli = new javax.swing.JButton();
 
@@ -56,13 +62,28 @@ public class ClientesFrame extends javax.swing.JFrame {
         LbApellidoPaternoCli.setText("Apellido Paterno");
 
         LbDireccionCli.setFont(new java.awt.Font("Rockwell", 0, 14)); // NOI18N
-        LbDireccionCli.setText("Direccion");
+        LbDireccionCli.setText("Calle");
 
         LbTelefonoCli.setFont(new java.awt.Font("Rockwell", 0, 14)); // NOI18N
         LbTelefonoCli.setText("Telefono");
 
         LbCorreoCli.setFont(new java.awt.Font("Rockwell", 0, 14)); // NOI18N
         LbCorreoCli.setText("Correo ");
+
+        TxtTelefonoCli.addActionListener(new java.awt.event.ActionListener() {
+            public void actionPerformed(java.awt.event.ActionEvent evt) {
+                TxtTelefonoCliActionPerformed(evt);
+            }
+        });
+
+        LbDireccionCli1.setFont(new java.awt.Font("Rockwell", 0, 14)); // NOI18N
+        LbDireccionCli1.setText("Colonia");
+
+        LbDireccionCli2.setFont(new java.awt.Font("Rockwell", 0, 14)); // NOI18N
+        LbDireccionCli2.setText("Pais");
+
+        LbDireccionCli3.setFont(new java.awt.Font("Rockwell", 0, 14)); // NOI18N
+        LbDireccionCli3.setText("Codigo postal");
 
         javax.swing.GroupLayout jPanel1Layout = new javax.swing.GroupLayout(jPanel1);
         jPanel1.setLayout(jPanel1Layout);
@@ -72,32 +93,47 @@ public class ClientesFrame extends javax.swing.JFrame {
                 .addGap(47, 47, 47)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addGroup(jPanel1Layout.createSequentialGroup()
-                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(LbNombreCIi)
-                            .addComponent(LbCorreoCli)
-                            .addComponent(LbDireccionCli)
-                            .addComponent(LbTelefonoCli))
+                        .addComponent(LbDireccionCli3)
+                        .addGap(0, 0, Short.MAX_VALUE))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
                         .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                             .addGroup(jPanel1Layout.createSequentialGroup()
+                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                    .addComponent(LbNombreCIi)
+                                    .addComponent(LbCorreoCli)
+                                    .addComponent(LbTelefonoCli))
                                 .addGap(65, 65, 65)
                                 .addComponent(TxtNombreCli, javax.swing.GroupLayout.PREFERRED_SIZE, 178, javax.swing.GroupLayout.PREFERRED_SIZE)
-                                .addGap(0, 0, Short.MAX_VALUE))
-                            .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
-                                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-                                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                                    .addComponent(TxtCorreoCli, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, 178, Short.MAX_VALUE)
-                                    .addComponent(TxtTelefonoCli, javax.swing.GroupLayout.Alignment.TRAILING)
-                                    .addComponent(TxtDireccionCli, javax.swing.GroupLayout.Alignment.TRAILING)))))
-                    .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
-                        .addGroup(jPanel1Layout.createSequentialGroup()
-                            .addComponent(LbApellidoPaternoCli)
-                            .addGap(18, 18, 18)
-                            .addComponent(txtApellidoPaterno))
-                        .addGroup(jPanel1Layout.createSequentialGroup()
-                            .addComponent(LbApellidoMaternoCli)
-                            .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
-                            .addComponent(TxtApellidoMaterno, javax.swing.GroupLayout.PREFERRED_SIZE, 179, javax.swing.GroupLayout.PREFERRED_SIZE))))
-                .addGap(25, 25, 25))
+                                .addGap(0, 41, Short.MAX_VALUE))
+                            .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                .addGroup(jPanel1Layout.createSequentialGroup()
+                                    .addComponent(LbApellidoPaternoCli)
+                                    .addGap(18, 18, 18)
+                                    .addComponent(txtApellidoPaterno))
+                                .addGroup(jPanel1Layout.createSequentialGroup()
+                                    .addComponent(LbApellidoMaternoCli)
+                                    .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                                    .addComponent(TxtApellidoMaterno, javax.swing.GroupLayout.PREFERRED_SIZE, 179, javax.swing.GroupLayout.PREFERRED_SIZE))))
+                        .addGap(25, 25, 25))
+                    .addGroup(jPanel1Layout.createSequentialGroup()
+                        .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.TRAILING)
+                            .addComponent(TxtTelefonoCli, javax.swing.GroupLayout.PREFERRED_SIZE, 178, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(TxtCorreoCli, javax.swing.GroupLayout.PREFERRED_SIZE, 178, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING, false)
+                                .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                                    .addComponent(LbDireccionCli)
+                                    .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+                                    .addComponent(TxtCalleCli1, javax.swing.GroupLayout.PREFERRED_SIZE, 178, javax.swing.GroupLayout.PREFERRED_SIZE))
+                                .addGroup(javax.swing.GroupLayout.Alignment.TRAILING, jPanel1Layout.createSequentialGroup()
+                                    .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                        .addComponent(LbDireccionCli1)
+                                        .addComponent(LbDireccionCli2))
+                                    .addGap(77, 77, 77)
+                                    .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                                        .addComponent(txtPaisCLT, javax.swing.GroupLayout.PREFERRED_SIZE, 178, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                        .addComponent(txtColoniaCLT1, javax.swing.GroupLayout.PREFERRED_SIZE, 178, javax.swing.GroupLayout.PREFERRED_SIZE)
+                                        .addComponent(txtCPCLT, javax.swing.GroupLayout.PREFERRED_SIZE, 178, javax.swing.GroupLayout.PREFERRED_SIZE)))))
+                        .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))))
         );
         jPanel1Layout.setVerticalGroup(
             jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -119,14 +155,26 @@ public class ClientesFrame extends javax.swing.JFrame {
                     .addComponent(LbTelefonoCli)
                     .addComponent(TxtTelefonoCli, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(TxtCalleCli1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(LbDireccionCli, javax.swing.GroupLayout.Alignment.TRAILING))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.UNRELATED)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+                    .addComponent(txtColoniaCLT1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                    .addComponent(LbDireccionCli1))
+                .addGap(22, 22, 22)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
-                    .addComponent(LbDireccionCli)
-                    .addComponent(TxtDireccionCli, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
+                    .addComponent(LbDireccionCli2)
+                    .addComponent(txtPaisCLT, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, 12, Short.MAX_VALUE)
                 .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.BASELINE)
+                    .addComponent(LbDireccionCli3)
+                    .addComponent(txtCPCLT, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
+                .addGap(18, 18, 18)
+                .addGroup(jPanel1Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
                     .addComponent(LbCorreoCli)
                     .addComponent(TxtCorreoCli, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE))
-                .addContainerGap(33, Short.MAX_VALUE))
+                .addGap(27, 27, 27))
         );
 
         jPanel2.setBorder(javax.swing.BorderFactory.createTitledBorder(null, "Acciones", javax.swing.border.TitledBorder.DEFAULT_JUSTIFICATION, javax.swing.border.TitledBorder.DEFAULT_POSITION, new java.awt.Font("Rockwell", 0, 12))); // NOI18N
@@ -140,7 +188,7 @@ public class ClientesFrame extends javax.swing.JFrame {
             .addGroup(jPanel2Layout.createSequentialGroup()
                 .addGap(38, 38, 38)
                 .addComponent(BtnGuardarCli, javax.swing.GroupLayout.PREFERRED_SIZE, 81, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap(327, Short.MAX_VALUE))
         );
         jPanel2Layout.setVerticalGroup(
             jPanel2Layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
@@ -155,19 +203,25 @@ public class ClientesFrame extends javax.swing.JFrame {
         layout.setHorizontalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addComponent(jPanel2, javax.swing.GroupLayout.Alignment.TRAILING, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-            .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+            .addGroup(layout.createSequentialGroup()
+                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addGap(0, 0, Short.MAX_VALUE))
         );
         layout.setVerticalGroup(
             layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
             .addGroup(layout.createSequentialGroup()
-                .addComponent(jPanel1, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
+                .addComponent(jPanel1, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                 .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED)
                 .addComponent(jPanel2, javax.swing.GroupLayout.PREFERRED_SIZE, javax.swing.GroupLayout.DEFAULT_SIZE, javax.swing.GroupLayout.PREFERRED_SIZE)
-                .addContainerGap(javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+                .addContainerGap())
         );
 
         pack();
     }// </editor-fold>//GEN-END:initComponents
+
+    private void TxtTelefonoCliActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_TxtTelefonoCliActionPerformed
+        // TODO add your handling code here:
+    }//GEN-LAST:event_TxtTelefonoCliActionPerformed
 
     /**
      * @param args the command line arguments
@@ -210,15 +264,21 @@ public class ClientesFrame extends javax.swing.JFrame {
     private javax.swing.JLabel LbApellidoPaternoCli;
     private javax.swing.JLabel LbCorreoCli;
     private javax.swing.JLabel LbDireccionCli;
+    private javax.swing.JLabel LbDireccionCli1;
+    private javax.swing.JLabel LbDireccionCli2;
+    private javax.swing.JLabel LbDireccionCli3;
     private javax.swing.JLabel LbNombreCIi;
     private javax.swing.JLabel LbTelefonoCli;
     private javax.swing.JTextField TxtApellidoMaterno;
+    private javax.swing.JTextField TxtCalleCli1;
     private javax.swing.JTextField TxtCorreoCli;
-    private javax.swing.JTextField TxtDireccionCli;
     private javax.swing.JTextField TxtNombreCli;
     private javax.swing.JTextField TxtTelefonoCli;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JTextField txtApellidoPaterno;
+    private javax.swing.JTextField txtCPCLT;
+    private javax.swing.JTextField txtColoniaCLT1;
+    private javax.swing.JTextField txtPaisCLT;
     // End of variables declaration//GEN-END:variables
 }
